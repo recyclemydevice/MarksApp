15a095971ca165a745af5de66232adb4990e6034
Mark <Mark@Mark-PC>
2014-11-02 16:44:40 +0000

    Made a slight change

--- src/com/example/marksapp/MainActivity.java
+++ src/com/example/marksapp/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
     public void toNextActivity() {
     	
+    	// Go to next activity
     	Intent i = new Intent(this, RandomNumber.class);
     	
     	startActivity(i);
